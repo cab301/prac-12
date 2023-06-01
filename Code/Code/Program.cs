78ee711fd85ebace2e6bc56dc370101c798d7144
Dan Tran <67458114+autumnssuns@users.noreply.github.com>
2023-06-01 18:33:57 +1000

    Add files via upload

---
+++ Code/Code/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruteForceStringMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = "SPEED";
+            string text = "THERE_IS_MORE_TO_LIFE_THAN_INCREASING_ITS_SPEED";
+
+            Console.Clear();
+            int result = BruteForceStringMatch(text, pattern);
+            if (result != -1)
+            {
+                Console.WriteLine("The number of comparisons made is " + result);
+            }
+            else
+            {
+                Console.WriteLine("The algorithm failed in doing the comparisons");
+            }
+
+            Console.ReadKey();
+        }
+
+        static int BruteForceStringMatch
+            (string text, string pattern)
+        {
+            int n = text.Length;
+            int m = pattern.Length;
+            for (int i = 0; i <= n - m; i++)
+            {
+                int j;
+                for (j = 0; j < m && pattern[j] == text[i + j]; j++) { }
+                if (j == m) return i;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Code/HorspoolAlgorithm/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorspoolAlgorithm
+{
+    class Program
+    {
+        static int MAX = 500;
+        static int[] shiftTable = new int[MAX];
+
+        static void ShiftTable(char[] pattern)
+        {
+            int i, j, m;
+            m = pattern.Length;
+            for (i = 0; i < MAX; i++)
+            {
+                shiftTable[i] = m;
+            }
+            for (j = 0; j < m - 1; j++)
+            {
+                shiftTable[pattern[j]] = m - 1 - j;
+            }
+        }
+
+        static int Horspool(char[] text, char[] pattern)
+        {
+            int m = pattern.Length;
+            int n = text.Length;
+            Console.WriteLine("Length of text = {0}", n);
+            Console.Write("Length of pattern = {0}", m);
+            for (int i = m - 1; i <= n - 1; i += shiftTable[text[i]])
+            {
+                int k;
+                for (k = 0; k <= m - 1 && pattern[m - 1 - k] == text[i - k]; k++) { }
+                if (k == m) return i - m + 1;
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            char[] text = new char[100];
+            char[] pattern = new char[100];
+            int position;
+
+            Console.Clear();
+            Console.Write("Enter the text in which pattern is to be searched: ");
+            text = Console.ReadLine().ToCharArray();
+            Console.Write("Enter the pattern to be searched: ");
+            pattern = Console.ReadLine().ToCharArray();
+
+            ShiftTable(pattern);
+            position = Horspool(text, pattern);
+            if (position >= 0)
+                Console.Write("\n The desired pattern was found starting from position {0}", position + 1);
+            else
+                Console.Write("\n The pattern was not found in the given text\n");
+            Console.ReadKey();
+        }
+    }
+}
